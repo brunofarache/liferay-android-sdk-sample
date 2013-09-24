1dc1bdcc821b05971ec509d3accf059637080aec
Bruno Farache <bruno.farache@liferay.com>
2013-09-24 14:07:47 -0300

    IDE-1173 - Remove copyright notices and author from sample app

--- src/com/liferay/mobile/sample/activity/MainActivity.java
+++ src/com/liferay/mobile/sample/activity/MainActivity.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package com.liferay.mobile.sample.activity;
 
 import java.util.ArrayList;
@@ -27,9 +13,6 @@ import android.app.ListActivity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 
-/**
- * @author Bruno Farache
- */
 @SuppressLint("NewApi")
 @SuppressWarnings("unchecked")
 public class MainActivity extends ListActivity {
--- src/com/liferay/mobile/sample/model/UserSite.java
+++ src/com/liferay/mobile/sample/model/UserSite.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package com.liferay.mobile.sample.model;
 
 import android.annotation.SuppressLint;
@@ -20,9 +6,6 @@ import java.io.Serializable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * @author Bruno Farache
- */
 @SuppressLint("DefaultLocale")
 @SuppressWarnings("serial")
 public class UserSite implements Comparable<UserSite>, Serializable {
